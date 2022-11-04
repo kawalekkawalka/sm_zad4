b70ca9e714c51ac94fa82bcc579ec8961ec09250
Hubert <hubertkawalko@gmail.com>
2022-11-04 15:01:20 +0100

    init

---
+++ app/src/main/java/com/example/todoapp/Task.java
@@ -0,0 +1,40 @@
+package com.example.todoapp;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Task {
+    private UUID id;
+    private String name;
+    private Date date;
+    private boolean done;
+
+    public Task(){
+        id = UUID.randomUUID();
+        date = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isDone(){
+        return done;
+    }
+
+    public void setDone(boolean isChecked) {
+        this.done = isChecked;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/TaskFragment.java
@@ -0,0 +1,77 @@
+package com.example.todoapp;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+public class TaskFragment extends Fragment {
+    private TextView nameField;
+    private Button dateButton;
+    private CheckBox doneCheckBox;
+    private Task task;
+    private static final String ARG_TASK_ID = "ARG_TASK_ID";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UUID taskId = (UUID) getArguments().getSerializable(ARG_TASK_ID);
+        task = TaskStorage.getInstance().getTask(taskId);
+
+    }
+
+    public static TaskFragment newInstance(UUID taskId){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ARG_TASK_ID, taskId);
+        TaskFragment taskFragment = new TaskFragment();
+        taskFragment.setArguments(bundle);
+        return taskFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        nameField = getView().findViewById(R.id.task_name);
+        nameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                task.setName(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
+
+        dateButton = getView().findViewById(R.id.task_date);
+        dateButton.setText(task.getDate().toString());
+        dateButton.setEnabled(false);
+
+        doneCheckBox = getView().findViewById(R.id.task_done);
+        doneCheckBox.setChecked(task.isDone());
+        doneCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> task.setDone(isChecked));
+        return view;
+    }
+
+}
